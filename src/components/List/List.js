c4dd1eb3a152d7b5f7ff9583f834ba92886c2635
jfgarciat <juanfegar1996@gmail.com>
2022-06-21 14:04:57 -0500

    Fix the comments inside of List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,30 @@ import TodoItem from './TodoItem.js/TodoItem'
 import classes from './List.module.css'
 
 
+
+const List = (props) => {
+    
+    const todoList = props.items.map((user) => (
+        <TodoItem
+        key={user.id}
+        id={user.id}
+        name={user.text}
+        />
+        ));
+        
+        return (
+            <section className={classes.todos}>
+            {todoList}
+        </section>
+    )
+}
+
+
+
+
+
+
+
 // const DUMMY_TODOS = [
 //     {
 //         id: 't1',
@@ -34,21 +58,4 @@ import classes from './List.module.css'
 //     )
 // };
 
-
-const List = (props) => {
-
-    const todoList = props.items.map((user) => (
-        <TodoItem
-        key={user.id}
-        id={user.id}
-        name={user.text}
-        />
-    ));
-
-    return (
-        <section className={classes.todos}>
-            {todoList}
-        </section>
-    )
-}
 export default List;
